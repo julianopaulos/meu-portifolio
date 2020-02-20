0c562b430d388654ae30ac31ee378ad24b41d6ae
Juliano <julianopaulo.santos@hotmail.com>
2020-02-20 10:04:11 -0300

    Updates

---
+++ precache-manifest.3ad371e04b4529294b5343ade72437e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "567615b37744c0dc36dfd2c721c61fb3",
+    "url": "/meu-portifolio/index.html"
+  },
+  {
+    "revision": "43c8a28863fa09a13f6f",
+    "url": "/meu-portifolio/static/js/2.0c328b6b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/meu-portifolio/static/js/2.0c328b6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff833c200a02fb82d505",
+    "url": "/meu-portifolio/static/js/main.e9408d37.chunk.js"
+  },
+  {
+    "revision": "427ecd20ca47fca63743",
+    "url": "/meu-portifolio/static/js/runtime-main.b689845a.js"
+  },
+  {
+    "revision": "6f2bd3168a950d25528065b9a99fba8d",
+    "url": "/meu-portifolio/static/media/fast_food.6f2bd316.jpg"
+  }
+]);
